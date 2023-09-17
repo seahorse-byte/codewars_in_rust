81a68ddd758c7ef8915cce53aeb364d81bdb491e
Olsi Gjeci <olsi.developer@gmail.com>
2023-09-17 14:15:55 +0100

    more examples in create_phone_number

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ mod challenges;
 
 fn main() {
     let answer = create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 9]);
+    // assert_eq!(create_phone_number(&[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
+    // assert_eq!(create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 9]), "(123) 456-7899");
     println!("answer is {}", answer);
 }
