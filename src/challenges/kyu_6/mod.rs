79d73dacf448ca90b1108e7b9d50238ce5354175
Olsi Gjeci <olsi.developer@gmail.com>
2023-09-17 23:03:58 +0100

    new kata incoming

--- src/challenges/kyu_6/mod.rs
+++ src/challenges/kyu_6/mod.rs
@@ -3,9 +3,6 @@ Write a function that accepts an array of 10 integers (between 0 and 9), that re
 ex. create_phone_number(&[1,2,3,4,5,6,7,8,9,0]); // returns "(123) 456-7890"
 The returned format must be correct in order to complete this challenge.
 */
-
-// use codewars_in_rust::concat_to_string;
-
 pub fn concat_to_string(slice: &[u8]) -> String {
     slice.iter().map(|&x| x.to_string()).collect()
 }
@@ -16,4 +13,31 @@ pub fn create_phone_number(numbers: &[u8]) -> String {
     let second_group: String = concat_to_string(&numbers[6..]);
 
     return format!("({}) {}-{}", area, first_group, second_group).to_string();
+}
+
+// ----------------------------------------------------------------
+// FIND THE ODD INT KATA
+// ----------------------------------------------------------------
+/*
+Given an array of integers, find the one that appears an odd number of times.
+There will always be only one integer that appears an odd number of times.
+*/
+
+#[allow(dead_code)]
+pub fn find_it(_seq: &[i32]) -> i32 {
+    // let mut count = 0;
+    // let mut odd = 0;
+    // for i in 0..seq.len() {
+    //     for j in 0..seq.len() {
+    //         if seq[i] == seq[j] {
+    //             count += 1;
+    //         }
+    //     }
+    //     if count % 2 != 0 {
+    //         odd = seq[i];
+    //         break;
+    //     }
+    // }
+    // return odd;
+    todo!()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ use crate::challenges::kyu_6::create_phone_number;
 mod challenges;
 
 fn main() {
+    // CREATE PHONE NUMBER KATA
     let answer = create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 9]);
-    // assert_eq!(create_phone_number(&[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
-    // assert_eq!(create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 9]), "(123) 456-7899");
-    println!("answer is {}", answer);
+    assert_eq!(create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 9]), answer);
+
+    // FIND THE ODD INT KATA
+    // let answer = find_it(&[1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5]);
+    // assert_eq!(find_it(&[1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5]), answer);
+    
 }
